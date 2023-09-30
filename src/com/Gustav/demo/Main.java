1b3bdef1412aef28af8bc191351f1697c4398623
GustavParment <gustav.parment1993@gmail.com>
2023-09-30 14:17:23 +0200

    break point

--- src/com/Gustav/demo/Main.java
+++ src/com/Gustav/demo/Main.java
@@ -1,5 +1,15 @@
 package com.Gustav.demo;
 
 public class Main {
+    public static void main(String[] args) {
+
+        int[] scores = {0, 155, 500};
+
+        for (int i = 0; i < scores.length ; i++) {
+            System.out.println(scores[i]);
+            
+        }
+        
+    }
 
 }
